a1694df87b5addc5ec677344b8db5ad9d8f09ee5
Kavit7 <kavitpaul7@gmail.com>
2026-09-13 17:44:10 +0300

    config url to request matcher

--- saas/src/main/java/com/saas/backend/config/SecurityConfig.java
+++ saas/src/main/java/com/saas/backend/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .cors(cors -> {})
             .authorizeHttpRequests(auth->auth
+              //this Url
             .requestMatchers("/api/v1/auth/**","/api/v1/**","/docs/**","/swagger-ui/**","/v3/api-docs/**")
             .permitAll()
             .anyRequest()
